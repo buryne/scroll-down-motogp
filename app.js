72e0c8275633db942b173497d0829669275f078a
aefars <aefarsmarine@gmail.com>
2022-06-23 09:32:50 +0700

    first main

---
+++ app.js
@@ -0,0 +1,21 @@
+const progressBar = document.querySelector("#progress-bar");
+const section = document.querySelector("section");
+
+const animateProgressBar = () => {
+  let scrollDistance = -section.getBoundingClientRect().top;
+  let progressWidth =
+    (scrollDistance /
+      (section.getBoundingClientRect().height -
+        document.documentElement.clientHeight)) *
+    100;
+
+  let value = Math.floor(progressWidth);
+  progressBar.style.width = `${value}%`;
+
+  console.log(value);
+
+  console.log(progressWidth);
+  // console.log(scrollDistance);
+};
+
+window.addEventListener("scroll", animateProgressBar);
